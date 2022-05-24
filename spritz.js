4a07805097f621d514eb5fd8bf0f270425412773
dnan657 <96710971+dnan657@users.noreply.github.com>
2022-05-25 01:11:37 +0600

    Update spritz.js

--- spritz.js
+++ spritz.js
@@ -1,17 +1,13 @@
 function spritz_word(word=""){
-		let result = {
-			start: "",
-			middle: "",
-			end: ""
-		};
-		
-		word += "";
-		let len = word.length;
-		let i_middle = Math.floor(len * 0.38);
-		
-		result.start = word.slice(0, i_middle);
-		result.middle = word.slice(i_middle, i_middle+1);
-		result.end = word.slice(i_middle+1);
-		
-		return result;
+	let result = {};
+
+	word += "";
+	let len = word.length;
+	let i_middle = Math.floor(len * 0.38);
+
+	result['start'] = word.slice(0, i_middle);
+	result['middle'] = word.slice(i_middle, i_middle+1);
+	result['end'] = word.slice(i_middle+1);
+
+	return result;
 }
